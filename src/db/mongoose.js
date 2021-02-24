445d58002cc63ed5ac230fecfc742558185d2199
Mai M.belal <mai.belal2127@gmail.com>
2021-02-24 23:30:53 +0200

    change-mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,37 +8,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/test',{
      useCreateIndex: true,
      useFindAndModify:false,
     });
-// const User = mongoose.model('User',{
-//     name:{
-//         type:String,
-//     },
-//     age:{
-//         type:Number
-//     }
-// })
-// const Task = mongoose.model('Task',{
-//     description:{
-//         type:String,
-//     },
-//     completed:{
-//         type:Boolean
-//     }
-// })
-
-//instance
-// const user = new User({
-//     name:'mohamed',
-//     age: 23,
-// });
-// const task = new Task({
-//     description:'task for mohamed',
-//     completed: true,
-// });
-// user.save().then(()=>{
-//     console.log(user);
-// }).catch((error) => console.log('error'));
-
-// task.save().then(()=>{
-//     console.log(task);
-// }).catch((error) => console.log('error'));
-
