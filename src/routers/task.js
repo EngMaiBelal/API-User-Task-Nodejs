283dfc0d594998caec124a8ae6f6a9e190e342fb
Mai M.belal <mai.belal2127@gmail.com>
2021-02-24 23:38:35 +0200

    Update task.js

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,88 +3,9 @@ const routerTask= new express.Router()
 const Task = require('../models/task')
 
 const auth=require('../middleware/auth.js')
-// ////////////////////////////////////
-// //post
-
-// routerTask.post('/tasks',(req,res)=>{
-//     const task = new Task(req.body)
-//     task.save().then(()=>{
-//         // to show in post man
-//         res.status(200).send(task)
-//     }).catch((error)=>{
-//         res.status(400).send(error)
-//     })
-// })
-// ////////////////////////////////
-// // Get
-// routerTask.get('/tasks',(req,res)=>{
-//     Task.find({}).then((task)=>{
-//         res.status(200).send(task)
-//     }).catch((error)=>{
-//         res.status(500).send("internal server error")
-//     })
-// })
 
 
-// ////////////////////
-// routerTask.get('/tasks/:id',(req,res)=>{
-//     console.log(req.params)
-//     const _id = req.params.id
-//     Task.findById(_id).then((task)=>{
-//         if(!task){
-//             return res.status(400).send('unable to send user')
-//         }
-//         res.status(200).send(task)
-//     }).catch((e)=>{
-//         res.status(500).send('internal server error')
-//     })
-
-// })
-
-
-// //////////////
-// routerTask.patch('/tasks/:id' ,async(req,res)=>{
-//     const _id=req.params.id
-//     try{
-
-//         const task = await Task.findByIdAndUpdate(_id,req.body,{
-//             new:true,
-//             runValidators:true
-//         })
-//         if(!task){
-//             return res.send('no task found')
-//         }
-//         res.status(200).send(task)
-        
-//     }catch{
-//         res.status(400).send('Error has occured')
-//     }
-// })
-
-
-
-
-
-
-
-
-////////////////////////////////////
-//post
-
-// routerTask.post('/tasks',async (req,res)=>{
-//     const task = new Task(req.body)
-    
-//     try{
-
-//         await task.save()
-//         res.status(200).send(task)
-    
-//     }catch(error){
-//         res.status(400).send(error)
-//     }
-// })
-
-////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //to add owner
 
@@ -104,28 +25,10 @@ routerTask.post('/tasks',auth,async (req,res)=>{
         res.status(400).send(error)
     }
 })
-////////////////////////////////
-// Get all
-// routerTask.get('/tasks',auth,async (req,res)=>{
-//     try{
-//         // 1st way
-//         // const tasks = await Task.find({})
-//         // const tasks = await Task.find({owner:req.user._id})
-//         // res.send(tasks)
 
-//         // 2nd way
-//         await req.user.populate('tasks').execPopulate()
-//         res.send(req.user.tasks)
-        
-//     }
-//     catch(error){
-//         res.status(500).send(error)
-//     }
-// })
-
-
-/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 // get by id
+
 routerTask.get('/tasks/:id',auth,async (req,res)=>{
     const _id = req.params.id
     try{
@@ -145,7 +48,7 @@ routerTask.get('/tasks/:id',auth,async (req,res)=>{
 })
 
 
-//////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 // edit by id
 
 routerTask.patch('/tasks/:id', auth,async(req,res)=>{
@@ -165,28 +68,10 @@ routerTask.patch('/tasks/:id', auth,async(req,res)=>{
         res.status(400).send(e)
     }
 })
-// routerTask.patch('/tasks/:id',auth ,async(req,res)=>{
-//     const _id=req.params.id
-//     const updates = Object.keys(req.body)
-//     try{
-//         // const task = await Task.findById(_id)
-//         const task = await Task.findOne({_id,owner:req.user._id})
-//         // const task = await Task.findOne({...req.params.id,owner:req.user._id})
 
-//         if(!task){
-//             return res.status(404).send('no task found')
-//         }
-//         updates.forEach((update)=>task[update]=req.body[update])
-//         await task.save()
-//         res.send(task)
-        
-//     }catch(e){
-//         // res.status(400).send('Error has occured')
-//         res.status(400).send(e)
+///////////////////////////////////////////////////////////////////////////////////
+// delete 
 
-//     }
-// })
-//////////////////////////
 routerTask.delete('/tasks/:id',auth,async(req,res)=>{
     const _id=req.params.id
     
@@ -209,9 +94,8 @@ routerTask.delete('/tasks/:id',auth,async(req,res)=>{
 
     }
 })
-/////////////////////////////////////////////
-// Get localhost:3000/tasks/?completed=false   --> string
-// Get /tasks/?query
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// match & sort
 
 routerTask.get('/tasks',auth,async(req,res)=>{
 try{
@@ -241,11 +125,5 @@ try{
     res.status(500).send(e)
 }
 })
-///////////////////////////////////////////
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = routerTask
